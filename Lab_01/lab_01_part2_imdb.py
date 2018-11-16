bd6465d2875c818c1163536f6650fd5402720d8c
Giannis Pitoskas <42012487+jpitoskas@users.noreply.github.com>
2018-11-16 20:09:15 +0200

    Added part of step 18

--- Lab_01/lab_01_part2_imdb.py
+++ Lab_01/lab_01_part2_imdb.py
@@ -2,6 +2,10 @@ import sys
 import os
 import re
 import itertools
+import gensim
+from gensim.test.utils import common_texts, get_tmpfile
+from gensim.models import Word2Vec
+from random import randint
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 # from sklearn import metrics
@@ -25,7 +29,7 @@ neg_test_dir = os.path.join(test_dir, 'neg')
 
 # For memory limitations. These parameters fit in 8GB of RAM. (5000)
 # If you have 16G of RAM you can experiment with the full dataset / W2V
-MAX_NUM_SAMPLES = 500
+MAX_NUM_SAMPLES = 2
 # Load first 1M word embeddings. This works because GoogleNews are roughly
 # sorted from most frequent to least frequent.
 # It may yield much worse results for other embeddings corpora
@@ -43,7 +47,7 @@ def tokenize(s):
     s = regex.sub(' ', s)
     s = s.replace('\n',' ')
     s = re.sub(' +',' ', s)
-    s = s.split(' ')
+    # s = s.split(' ')
     return s
 
 def read_samples(folder, preprocess=lambda x: x):
@@ -66,10 +70,14 @@ def create_corpus(pos, neg):
 
 def simple_lr_classify(X_tr, y_tr, X_test, y_test, description):
     # Helper function to train a logistic classifier and score on test data
-    clf_LR = LogisticRegression().fit(X_tr, y_tr)
-    score = clf_LR.score(X_test, y_test)
-    print('Test score with', description, ': ', score)
-    return clf_LR
+    clf_LR = LogisticRegression(solver='liblinear').fit(X_tr, y_tr)
+    # clf_LR.predict(X_test)
+    print('Test score with', description, ': ', clf_LR.score(X_test, y_test))
+    # return clf_LR
+
+# Load pretrained w2v model from Pre Lab 01
+model = Word2Vec.load('word2vec.model')
+voc = model.wv.index2word
 
 # Load train sets
 neg_train = read_samples(neg_train_dir, tokenize)
@@ -85,17 +93,29 @@ corpus_train = create_corpus(pos_train, neg_train)
 # Create corpus from test sets
 corpus_test = create_corpus(pos_test, neg_test)
 
-x_train = corpus_train[0]
+# corpus_train_joined = list(itertools.chain.from_iterable(corpus_train[0]))
+
+# print(x_train[1])
+
+# Transform using Count Vectorizer for train
+BoW_train = CountVectorizer().fit_transform(corpus_train[0])
+
+# Transform using Count Vectorizer for test
+BoW_test = CountVectorizer().fit_transform(corpus_test[0])
+
+x_train = BoW_train
 y_train = corpus_train[1]
 
-corpus_train_joined = list(itertools.chain.from_iterable(corpus_train[0]))
+# print(x_train.shape)
+# print(len(y_train))
 
-# print(corpus_train_joined)
+x_test = BoW_test
+y_test = corpus_test[1]
 
-# Transform using Count Vectorizer
-BoW = CountVectorizer().fit_transform(corpus_train_joined)
+# print(x_test.shape)
+# print(len(y_test))
 
-# clf_LR = LogisticRegression().fit(x_train, y_train)
+simple_lr_classify(x_train, y_train, x_test, y_test, "bourda")
 
 # Transform using tf-idf Vectorizer
 # BoW = TfidfVectorizer().fit_transform(corpus_train[0])
