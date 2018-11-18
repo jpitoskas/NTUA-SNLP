e57164b0ac81894bd73ddcace2d5a7a356f74708
Giannis Pitoskas <42012487+jpitoskas@users.noreply.github.com>
2018-11-18 19:12:14 +0200

    Modified lab_01_par2_imdb.py - Loaded pretrained model over GoogleNews and work on it

--- Lab_01/lab_01_part2_imdb.py
+++ Lab_01/lab_01_part2_imdb.py
@@ -171,3 +171,20 @@ for i in range(len(corpus_test[0])):
 
 # Call train and evaluate function for Word2Vec representation
 clf_cntVect = simple_lr_classify(NBoW_train, y_train, NBoW_test, y_test, "Word2Vec")
+
+# Load pretrained Google model over all GoogleNews
+google_model = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True, limit=NUM_W2V_TO_LOAD)
+google_voc = google_model.wv.index2word
+
+# For 10 random words in vocabulary
+for i in range(10):
+    rnd = randint(0, len(voc))
+    while (not(voc[rnd] in google_voc)):
+        rnd = randint(0, len(voc))
+    # get most similar word
+    sim = model.wv.most_similar(voc[rnd])
+    sim_google = google_model.wv.most_similar(voc[rnd])
+    print('For word: '+voc[rnd])
+    print('Most similar (in text): '+sim[0][0])
+    print('Most similar (Google): '+sim_google[0][0])
+    print()
