f70e8d08167b7ba26fad850263f1ade6437a3741
Giannis Pitoskas <42012487+jpitoskas@users.noreply.github.com>
2018-11-18 19:05:39 +0200

    Modified lab_01_part2_imdb.py - Train with W2V representation using NBOW

--- Lab_01/lab_01_part2_imdb.py
+++ Lab_01/lab_01_part2_imdb.py
@@ -4,16 +4,12 @@ import re
 import itertools
 import gensim
 from gensim.test.utils import common_texts, get_tmpfile
-from gensim.models import Word2Vec
-from random import randint
+from gensim.models import Word2Vec, KeyedVectors
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-# from sklearn import metrics
-# from sklearn.metrics import classification_report
-# from sklearn.metrics import confusion_matrix
-# import subprocess
 from math import *
 import numpy as np
+from random import randint
 try:
     import glob2 as glob
 except ImportError:
@@ -29,7 +25,7 @@ neg_test_dir = os.path.join(test_dir, 'neg')
 
 # For memory limitations. These parameters fit in 8GB of RAM. (5000)
 # If you have 16G of RAM you can experiment with the full dataset / W2V
-MAX_NUM_SAMPLES = 1000
+MAX_NUM_SAMPLES = 5000
 # Load first 1M word embeddings. This works because GoogleNews are roughly
 # sorted from most frequent to least frequent.
 # It may yield much worse results for other embeddings corpora
@@ -78,18 +74,6 @@ def simple_lr_classify(X_tr, y_tr, X_test, y_test, description):
     print('Test score with', description, ': ', clf_LR.score(X_test, y_test))
     return clf_LR
 
-def test_classification(corpus_train, corpus_test, ):
-    corpus = corpus_train[0]
-    vectorizer = CountVectorizer()
-    # vectorizer.fit_transform(corpus).todense()
-    # vectorizer.vocabulary_
-    y_train = corpus_train[1]
-    X_train = vectorizer.fit_transform(corpus)
-    classifier = LogisticRegression(solver='liblinear')
-    clf = classifier.fit(X_train, y_train)
-    X_test = vectorizer.transform(corpus_test[0])
-    print('Test score with', description, ': ', clf.score(X_test, y_test))
-
 # Load train sets
 neg_train = read_samples(neg_train_dir, tokenize)
 pos_train = read_samples(pos_train_dir, tokenize)
@@ -104,75 +88,86 @@ corpus_train = create_corpus(pos_train, neg_train)
 # Create corpus from test sets
 corpus_test = create_corpus(pos_test, neg_test)
 
-# print(len(corpus_train[0]))
 
-# test_classification(corpus_train, corpus_test, "Count Vectorizer")
+# Count Vectorizer
+
+
+cntVect = CountVectorizer()
 
 # Transform using Count Vectorizer for train
-cntVect = CountVectorizer()
 BoW_cntVect_train = cntVect.fit_transform(corpus_train[0])
 voc_train = list(cntVect.vocabulary_.keys())
-
 
 # Transform using Count Vectorizer for test
 BoW_cntVect_test = cntVect.transform(corpus_test[0])
 voc_test = list(cntVect.vocabulary_.keys())
 
+# Set train parameters
 x_train = BoW_cntVect_train
 y_train = corpus_train[1]
 
-# print(x_train.shape)
-# print(len(y_train))
-
+# Set test parameters
 x_test = BoW_cntVect_test
 y_test = corpus_test[1]
 
-# print(x_test.shape)
-# print(len(y_test))
-
+# Call train and evaluate function for Count Vectorizer
 clf_cntVect = simple_lr_classify(x_train, y_train, x_test, y_test, "Count Vectorizer")
-# print(clf)
+
+
+# Tf-iDf Vectorizer
+
+
+TfiDfVect = TfidfVectorizer()
 
 # Transform using tf-idf for train
-TfiDfVect = TfidfVectorizer()
 BoW_TfiDf_train = TfiDfVect.fit_transform(corpus_train[0])
 
 # Transform using tf-idf for test
 BoW_TfiDf_test = TfiDfVect.transform(corpus_test[0])
 
+# Set train parameters
 x_train = BoW_TfiDf_train
+# Set test parameters
 x_test = BoW_TfiDf_test
 
+# Call train and evaluate function for Tf-iDf Vectorizer
 clf_TfiDf = simple_lr_classify(x_train, y_train, x_test, y_test, "Tf-iDf")
 
+
 # Load pretrained w2v model from Pre Lab 01
+# let vocabulary be the one in Step 9
 model = Word2Vec.load('word2vec.model')
 voc = model.wv.index2word
 
+# Find percentage of "Out of Vocabulary" words
 oov_cnt = 0
 for word in voc_test:
     if word not in voc:
         oov_cnt += 1
 oov = 100 * oov_cnt / len(voc_test)
-# print('Percentage of OOV words: ' + str(oov) + ' %')
+print('Percentage of OOV words: ' + str(oov) + ' %')
 
-corpus = [
-'All my cats in a row row',
-'When my cat sits down, she looks like a Furby toy!',
-'The cat from outer space',
-'Sunshine loves to sit like this for some reason.'
-]
+# Transform using Word2Vec representation for train
+NBoW_train = np.zeros((len(corpus_train[0]), model.vector_size))
+for i in range(len(corpus_train[0])):
+    comment = corpus_train[0][i]
+    words = comment.split(' ')
+    repr = np.zeros(model.vector_size)
+    for word in words:
+        if (word in voc):
+            repr = repr + model.wv[word]
+    NBoW_train[i] = repr/len(words)
 
-# corpus = corpus_train[0]
-vectorizer = CountVectorizer()
-print(vectorizer.fit_transform(corpus).todense())
-# # vectorizer.vocabulary_
-# y_train = corpus_train[1]
-# X_train = vectorizer.fit_transform(corpus)
-# classifier = LogisticRegression(solver='liblinear')
-# clf = classifier.fit(X_train, y_train)
-# X_test = vectorizer.transform(corpus_test[0])
-# print()
-# print(clf.score(X_test, y_test))
-# predictions = classifier.predict(X_test)
-# print(predictions)
+# Transform using Word2Vec representation for test
+NBoW_test = np.zeros((len(corpus_test[0]), model.vector_size))
+for i in range(len(corpus_test[0])):
+    comment = corpus_test[0][i]
+    words = comment.split(' ')
+    repr = np.zeros(model.vector_size)
+    for word in words:
+        if (word in voc):
+            repr = repr + model.wv[word]
+    NBoW_test[i] = repr/len(words)
+
+# Call train and evaluate function for Word2Vec representation
+clf_cntVect = simple_lr_classify(NBoW_train, y_train, NBoW_test, y_test, "Word2Vec")
--- Lab_01/wtv_google.py
+++ Lab_01/wtv_google.py
@@ -9,21 +9,30 @@ from random import randint
 
 NUM_W2V_TO_LOAD = 1000000
 
+# Convert to numpy 2d array (n_vocab x vector_size)
+def to_embeddings_Matrix(model):
+    embedding_matrix = np.zeros((len(model.wv.vocab), model.vector_size))
+    word2idx = {}
+    for i in range(len(model.wv.vocab)):
+        embedding_matrix[i] = model.wv[model.wv.index2word[i]]
+        word2idx[model.wv.index2word[i]] = i
+        return embedding_matrix, model.wv.index2word, word2idx
+
 model = Word2Vec.load('word2vec.model')
 
 # get ordered vocabulary list
 voc = model.wv.index2word
 
 google_model = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True, limit=NUM_W2V_TO_LOAD)
-google_voc = model.wv.index2word
-
+google_voc = google_model.wv.index2word
+# print(google_voc)
 # get vector size
 dim = model.vector_size
 
 # For 10 random words in vocabulary
 for i in range(10):
     rnd = randint(0, len(voc))
-    while voc[rnd] not in google_voc:
+    while (not(voc[rnd] in google_voc)):
         rnd = randint(0, len(voc))
     # get most similar word
     sim = model.wv.most_similar(voc[rnd])
@@ -33,11 +42,11 @@ for i in range(10):
     print('Most similar (Google): '+sim_google[0][0])
     print()
 
-# Convert to numpy 2d array (n_vocab x vector_size)
-def to_embeddings_Matrix(model):
-    embedding_matrix = np.zeros((len(model.wv.vocab), model.vector_size))
-    word2idx = {}
-    for i in range(len(model.wv.vocab)):
-        embedding_matrix[i] = model.wv[model.wv.index2word[i]]
-        word2idx[model.wv.index2word[i]] = i
-    return embedding_matrix, model.wv.index2word, word2idx
+# print(model.wv['india'] == model['india'])
+corpus = [  voc[1] + ' ' + voc[5] + ' ' + voc[80] + ' ' + voc[178],
+            voc[17] + ' ' + voc[42],
+            voc[324] + ' ' + voc[284] + ' ' + voc[1500] + ' ' + voc[312]  ]
+
+embedding_matrix, voc, word2idx = to_embeddings_Matrix(model)
+
+for i in
