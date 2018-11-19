d431eeb652c74e07acb15c2f310a8930b5e46060
Antonis Papaoikonomou <36318735+antonypap@users.noreply.github.com>
2018-11-19 13:25:22 +0200

    Modified evaluation process

---
+++ Lab_01/create_test_set.py
@@ -0,0 +1,41 @@
+import sys
+import os
+import re
+
+def identity_preprocess(string):
+    return string
+
+def read_test(path, preprocess = None):
+    if preprocess is None:
+        preprocess = identity_preprocess
+    processed = []
+    with open(path, 'r') as f:
+        line = f.readline()
+        # print(line)
+        while line:
+            ps = preprocess(line)
+            processed += ps[1:]
+            line = f.readline()
+        return processed
+
+def tokenize(s):
+    s = s.strip()
+    s = s.lower()
+    # Keep lower/upper case characters, numbers and spaces
+    regex = re.compile('[^a-z ]')
+    s = regex.sub(' ', s)
+    s = s.replace('\n',' ')
+    s = re.sub(' +',' ', s)
+    s = s.split(' ')
+    return s
+
+def create_test_file(words, output):
+    testing = open(output, 'w')
+    for word in words:
+        testing.write(word+'\n')
+    testing.close()
+
+test = read_test(sys.argv[1], tokenize)
+# print(test)
+
+create_test_file(test, 'test_set.txt')
