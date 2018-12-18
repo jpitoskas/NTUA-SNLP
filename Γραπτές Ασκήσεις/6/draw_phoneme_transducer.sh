31a604ca24ab2cc225e398d927ed0b6896e47371
Giannis Pitoskas <42012487+jpitoskas@users.noreply.github.com>
2018-12-18 05:42:04 +0200

    added more things

--- Γραπτές Ασκήσεις/6/draw_phoneme_transducer.sh
+++ Γραπτές Ασκήσεις/6/draw_phoneme_transducer.sh
@@ -6,6 +6,8 @@ python3 askisi_6.py
 fstcompile --isymbols=chars.syms --osymbols=chars.syms transducer.txt > transducer.fst
 
 fstclosure transducer.fst > transducer_closure.fst
+fstrmepsilon transducer_closure.fst | fstdeterminize | fstminimize > transducer_test.fst
+# fstarcsort acceptor_opt.fst > acceptor_opt_sorted.fst
 # Draw Levenshtein Transducer
-fstdraw --isymbols=chars.syms --osymbols=chars.syms -portrait transducer.fst | dot -Tjpg > transducer.jpg
-fstdraw --isymbols=chars.syms --osymbols=chars.syms -portrait transducer_closure.fst | dot -Tjpg > transducer_closure.jpg
+fstdraw --isymbols=chars.syms --osymbols=chars.syms -portrait transducer.fst | dot -Gdpi=1000 -Tjpg > transducer.jpg
+fstdraw --isymbols=chars.syms --osymbols=chars.syms -portrait transducer_closure.fst | dot -Gdpi=1000 -Tjpg > transducer_closure.jpg
