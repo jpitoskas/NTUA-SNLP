a6b3ad759f7974379b3f029a44a0882f1f0aabde
Antonis Papaoikonomou <36318735+antonypap@users.noreply.github.com>
2018-12-18 05:14:50 +0200

    Modified part 5

--- Γραπτές Ασκήσεις/5/draw_levenshtein_transducer.sh
+++ Γραπτές Ασκήσεις/5/draw_levenshtein_transducer.sh
@@ -4,6 +4,7 @@ python3 ex5_levenshtein_transducer.py
 
 # Create Levenshtein Transducer
 fstcompile --isymbols=chars.syms --osymbols=chars.syms levenshtein_transducer.txt > levenshtein_transducer.fst
+# fstrmepsilon levenshtein_transducer_first.fst | fstdeterminize | fstminimize | fstarcsort > levenshtein_transducer.fst
 
 # Draw Levenshtein Transducer
 fstdraw --isymbols=chars.syms --osymbols=chars.syms -portrait levenshtein_transducer.fst | dot -Gdpi=400 -Tjpg > levenshtein_transducer.jpg
@@ -13,11 +14,9 @@ fstcompile --isymbols=chars.syms --osymbols=chars.syms acceptor.txt > acceptor.f
 fstrmepsilon acceptor.fst | fstdeterminize | fstminimize > acceptor_opt.fst
 fstarcsort acceptor_opt.fst > acceptor_opt_sorted.fst
 
-# Draw acceptor
-fstdraw --isymbols=chars.syms --osymbols=chars.syms -portrait acceptor_opt_sorted.fst | dot -Gdpi=400 -Tjpg > acceptor.jpg
-
 # Create testing FSTs
 fstcompile --isymbols=chars.syms --osymbols=chars.syms input.txt > input.fst
+# fstrmepsilon input_first.fst | fstdeterminize | fstminimize | fstarcsort > input.fst
 
 # Compose acceptor and converter and sort afterwards to create orthograph
 fstcompose input.fst levenshtein_transducer.fst > input_conv.fst
@@ -32,3 +31,16 @@ fstarcsort ortho_input.fst > ortho_input_sorted.fst
 fstdraw --isymbols=chars.syms --osymbols=chars.syms -portrait ortho_input_sorted.fst | dot -Gdpi=400 -Tjpg > ortho_input.jpg
 # Draw composed transducer with orthograph and test
 # fstdraw --isymbols=chars.syms --osymbols=chars.syms -portrait ortho_input_sorted.fst | dot -Tjpg > ortho_input.jpg
+
+# Find minimum distance
+fstshortestpath ortho_input_sorted.fst > min_dist.fst
+
+# Draw/Print minimum distance transducer
+fstdraw --isymbols=chars.syms --osymbols=chars.syms -portrait min_dist.fst | dot -Gdpi=400 -Tjpg > min_dist.jpg
+
+# Find minimum distance
+fstshortestpath -nshortest=5 ortho_input_sorted.fst > second_min_dist_f.fst
+fstrmepsilon second_min_dist_f.fst > second_min_dist.fst
+
+# Draw/Print minimum distance transducer
+fstdraw --isymbols=chars.syms --osymbols=chars.syms -portrait second_min_dist.fst | dot -Gdpi=400 -Tjpg > second_min_dist.jpg
