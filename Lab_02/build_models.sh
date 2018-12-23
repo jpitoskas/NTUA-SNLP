2b3a9bb373376be00078dbb656c53e427914e4f8
Antonis Papaoikonomou <36318735+antonypap@users.noreply.github.com>
2018-12-23 15:31:58 +0200

    Modified lang prep

--- Lab_02/build_models.sh
+++ Lab_02/build_models.sh
@@ -3,7 +3,7 @@ source ./kaldi-master/egs/wsj/s5/path.sh
 # dir = $pwd/kaldi-master/egs/usc/data/local
 # langdir = $pwd/kaldi-master/egs/usc/data/lang
 
-python3 omeleta.py
+# python3 omeleta.py
 
 build-lm.sh -i ./kaldi-master/egs/usc/data/local/dict/lm_train.text -n 1 -o ./kaldi-master/egs/usc/data/local/lm_tmp/built_lm_unigram.ilm.gz
 build-lm.sh -i ./kaldi-master/egs/usc/data/local/dict/lm_train.text -n 2 -o ./kaldi-master/egs/usc/data/local/lm_tmp/built_lm_bigram.ilm.gz
@@ -12,5 +12,5 @@ compile-lm ./kaldi-master/egs/usc/data/local/lm_tmp/built_lm_unigram.ilm.gz -t=y
 compile-lm ./kaldi-master/egs/usc/data/local/lm_tmp/built_lm_bigram.ilm.gz -t=yes /dev/stdout | grep -v unk | gzip -c > ./kaldi-master/egs/usc/data/local/nist_lm/compiled_lm_bigram.arpa.gz
 
 cd ./kaldi-master/egs/wsj/s5
-utils/prepare_lang.sh ../../usc/data/local/dict '!sil' ../../usc/data/lang ../../usc/data/lang
+utils/prepare_lang.sh ../../usc/data/local/dict '<oov>' ../../usc/data/lang ../../usc/data/lang
 # export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
