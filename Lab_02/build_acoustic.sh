3ec487c77f86d385f3a18c8a5e947a34bf7036ca
Giannis Pitoskas <42012487+jpitoskas@users.noreply.github.com>
2018-12-24 00:08:59 +0200

    Merging

--- Lab_02/build_acoustic.sh
+++ Lab_02/build_acoustic.sh
@@ -3,7 +3,9 @@ source ./kaldi-master/egs/usc/path.sh
 cd ./kaldi-master/egs/usc/
 
 # MFCCs
-for ste in train dev test; do
+# for ste in train dev test; do
+for ste in train; do
+  utils/fix_data_dir.sh data/$ste
   steps/make_mfcc.sh data/$ste
   # steps/compute_cmvn_stats.sh
 done
