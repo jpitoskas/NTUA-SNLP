9fbc64aee2062b51ba022d75cf472d9c8bfa8f5d
Giannis Pitoskas <42012487+jpitoskas@users.noreply.github.com>
2018-12-24 08:36:14 +0200

    peos

--- Lab_02/build_acoustic.sh
+++ Lab_02/build_acoustic.sh
@@ -6,5 +6,6 @@ cd ./kaldi-master/egs/usc/
 for ste in train dev test; do
   utils/fix_data_dir.sh data/$ste
   steps/make_mfcc.sh data/$ste
+  # steps/make_mfcc.sh --nj 20 --cmd scripts/run.pl data/train exp/make_mfcc/train /home
   # steps/compute_cmvn_stats.sh
 done
